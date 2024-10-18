e45ecc5a51eba58711ab7aab35bb57b051ba69a1
osunlajajamiu97 <osunlajajamiu97@gmail.com>
2024-10-18 16:03:49 +0100

    new commit

--- src/Pages/Home/Login.tsx
+++ src/Pages/Home/Login.tsx
@@ -59,7 +59,7 @@ const Login = () => {
       <Navbar />
       <Bubbles />
       <div className="flex container mx-auto px-5 py-10 items-center gap-10 justify-center md:gap-[250px] flex-col-reverse md:flex-row">
-        <div className="">
+        <div>
           <img src={note3} alt="signup" width={200} className="mx-auto" />
         </div>
         <div>
