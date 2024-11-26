4c2ba49b5a8b443b037c74595a3ea464444711b6
kenesongodwin@gmail.com <kenesongodwin@gmail.com>
2024-11-25 18:25:41 -0800

    Godwin

--- src/Components/ui/Bubbles/Bubbles.tsx
+++ src/Components/ui/Bubbles/Bubbles.tsx
@@ -1,15 +1,15 @@
-import './Bubbles.css'
+import "./Bubbles.css";
 
 const Bubbles = () => {
-    const bubblesArray = Array.from({ length: 20 });
+  const bubblesArray = Array.from({ length: 20 });
 
-    return (
-      <div className="bubbles-container">
-        {bubblesArray.map((_, index) => (
-          <div className="bubble" key={index}></div>
-        ))}
-      </div>
-    );
-}
+  return (
+    <div className="bubbles-container">
+      {bubblesArray.map((_, index) => (
+        <div className="bubble" key={index}></div>
+      ))}
+    </div>
+  );
+};
 
-export default Bubbles
+export default Bubbles;
